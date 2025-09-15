e829173a11fbd4e6ac55a33c2c83ba49e0e841b9
jeaye <contact@jeaye.com>
2025-09-15 15:51:44 -0700

    Fix PCH includes for `clang::Interpreter`

--- clang/lib/Interpreter/Interpreter.cpp
+++ clang/lib/Interpreter/Interpreter.cpp
@@ -278,7 +278,8 @@ Interpreter::Interpreter(std::unique_ptr<CompilerInstance> Instance,
     Act->CacheCodeGenModule();
     // The initial PTU is filled by `-include` or by CUDA includes
     // automatically.
-    if (!CI->getPreprocessorOpts().Includes.empty()) {
+    if (!CI->getPreprocessorOpts().Includes.empty() ||
+        !CI->getPreprocessorOpts().ImplicitPCHInclude.empty()) {
       // We can't really directly pass the CachedInCodeGenModule to the Jit
       // because it will steal it, causing dangling references as explained in
       // Interpreter::Execute
